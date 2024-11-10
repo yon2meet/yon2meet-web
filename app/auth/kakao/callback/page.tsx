4c99325dc421d7e4aaf231a46fd2c4c9725a7dc0
jhyunwoo <jhyunwoo0228@gmail.com>
2024-11-11 08:34:10 +0900

    upgrade packages

---
+++ app/auth/kakao/callback/page.tsx
@@ -0,0 +1,7 @@
+export default function KakaoCallbackPage() {
+  return (
+    <div>
+      <div>Kakao Callback Page</div>
+    </div>
+  );
+}
---
+++ app/auth/sign-in.tsx
@@ -0,0 +1,7 @@
+export default function SignInPage() {
+  return (
+    <div>
+      <div>Sign In Page</div>
+    </div>
+  );
+}
